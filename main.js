b5afc89a636a794833c72c5d6c1037a5d8115030
pingmommy <mcar8764@naver.com>
2024-06-08 20:43:24 +0900

    swiper 적용

---
+++ main.js
@@ -0,0 +1,9 @@
+const swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  effect: "fade",
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
